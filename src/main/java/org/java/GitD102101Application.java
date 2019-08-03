03bb4d47efaa4aa7459b3d39839b3ffa20e48083
1016432252 <1016432252@qq.com>
2019-08-03 11:46:00 +0800

    第一次提交

---
+++ src/main/java/org/java/GitD102101Application.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitD102101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitD102101Application.class, args);
+    }
+
+}
---
+++ src/main/java/org/java/web/GitController.java
@@ -0,0 +1,19 @@
+package org.java.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author：邱泽英
+ * @Date：2019/8/3 11:27
+ * @Description：文件
+ */
+@RestController
+public class GitController {
+
+    @GetMapping("init")
+    public String init() {
+
+        return "hello...";
+    }
+}
